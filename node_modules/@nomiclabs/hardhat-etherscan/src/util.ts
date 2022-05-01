bb861a1926e6cebb4a3c7f96e0b42190a2d676f6
beans <certfiedbagholder@gmail.com>
2022-05-01 15:07:06 -0700

    rm hardhat from getting tracked

--- node_modules/@nomiclabs/hardhat-etherscan/src/util.ts
+++
@@ -1,8 +0,0 @@
-export function buildContractUrl(
-  browserURL: string,
-  contractAddress: string
-): string {
-  const normalizedBrowserURL = browserURL.trim().replace(/\/$/, "");
-
-  return `${normalizedBrowserURL}/address/${contractAddress}#code`;
-}
